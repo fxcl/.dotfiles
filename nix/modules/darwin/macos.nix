631bdeff1c339520afdf58723a1cd567a77bcd5d
okk <me@gnux.cn>
2023-12-02 15:49:14 +0800

    。

--- nix/modules/darwin/macos.nix
+++ nix/modules/darwin/macos.nix
@@ -55,29 +55,47 @@ in
       };
 
        system = {
+        # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+        activationScripts.postUserActivation.text = ''
+          # activateSettings -u will reload the settings from the database and apply them to the current session,
+          # so we do not need to logout and login again to make the changes take effect.
+          /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+        '';
+
         defaults = {
-          # ".GlobalPreferences".com.apple.sound.beep.sound = "Funk";
-          LaunchServices.LSQuarantine = false;
-          SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
-          loginwindow.GuestEnabled = false;
-          NSGlobalDomain = {
+            menuExtraClock.Show24Hour = true; # show 24 hour clock
+            # ".GlobalPreferences".com.apple.sound.beep.sound = "Funk";
+            LaunchServices.LSQuarantine = false;
+            SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+            NSGlobalDomain = {
+            # `defaults read NSGlobalDomain "xxx"`
+            "com.apple.swipescrolldirection" = true; # enable natural scrolling(default to true)
+            "com.apple.sound.beep.feedback" = 0; # disable beep sound when pressing volume up/down key
+            AppleInterfaceStyle = "Dark"; # dark mode
+            AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
+            ApplePressAndHoldEnabled = false; # enable press and hold
+            # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
+            # This is very useful for vim users, they use `hjkl` to move cursor.
+            # sets how long it takes before it starts repeating.
+            InitialKeyRepeat = 15; # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+            # sets how fast it repeats once it starts.
+            KeyRepeat = 3; # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+
+            NSAutomaticCapitalizationEnabled = false; # disable auto capitalization(自动大写)
+            NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution(智能破折号替换)
+            NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution(智能句号替换)
+            NSAutomaticQuoteSubstitutionEnabled = false; # disable auto quote substitution(智能引号替换)
+            NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction(自动拼写检查)
+            NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default(保存文件时的路径选择/文件名输入页)
+            NSNavPanelExpandedStateForSaveMode2 = true;
+
             AppleFontSmoothing = 2;
-            AppleKeyboardUIMode = 3;
             AppleMeasurementUnits = "Centimeters";
             AppleMetricUnits = 1;
-            ApplePressAndHoldEnabled = false;
             AppleShowAllExtensions = true;
             AppleShowScrollBars = "Automatic";
             AppleTemperatureUnit = "Celsius";
-            InitialKeyRepeat = 10;
-            KeyRepeat = 1;
-            NSAutomaticCapitalizationEnabled = false;
-            NSAutomaticDashSubstitutionEnabled = false;
-            NSAutomaticQuoteSubstitutionEnabled = false;
-            NSAutomaticSpellingCorrectionEnabled = false;
             NSDocumentSaveNewDocumentsToCloud = false;
-            NSNavPanelExpandedStateForSaveMode = true;
-            NSNavPanelExpandedStateForSaveMode2 = true;
             NSTableViewDefaultSizeMode = 2;
             NSTextShowsControlCharacters = true;
             NSWindowResizeTime = 0.001;
@@ -85,7 +103,6 @@ in
             PMPrintingExpandedStateForPrint2 = true;
             _HIHideMenuBar = false;
             # com.apple.mouse.tapBehavior = 1;
-            # com.apple.sound.beep.feedback = 0;
             # com.apple.springing.delay = 0;
             # com.apple.springing.enabled = true;
           };
@@ -94,20 +111,28 @@ in
             autohide = false;
             autohide-delay = 0.0;
             autohide-time-modifier = 0.0;
+            show-recents = false;
+            # do not automatically rearrange spaces based on most recent use.
+            mru-spaces = false;
+
             dashboard-in-overlay = true;
-            expose-animation-duration = 0.1;
+            expose-animation-duration = 0.15;
             expose-group-by-app = false;
             launchanim = false;
             mineffect = "genie";
             minimize-to-application = true;
             mouse-over-hilite-stack = true;
             show-process-indicators = false;
-            show-recents = false;
             showhidden = true;
             static-only = true;
             tilesize = 46;
             # Hot corners, reset them all.
             # Not supported in nix-darwin yet
+            wvous-tl-corner = 2; # top-left - Mission Control
+            wvous-tr-corner = 13; # top-right - Lock Screen
+            wvous-bl-corner = 3; # bottom-left - Application Windows
+            wvous-br-corner = 4; # bottom-right - Desktop
+
             # wvous-tl-corner = 0;
             # wvous-tl-modifier = 0;
             # wvous-tr-corner = 0;
@@ -119,14 +144,66 @@ in
           };
 
           finder = {
-            AppleShowAllExtensions = true;
-            # QuitMenuItem = true;
-            _FXShowPosixPathInTitle = false; # In Big Sur this is so UGLY!
+           _FXShowPosixPathInTitle = true; # show full path in finder title
+            AppleShowAllExtensions = true; # show all file extensions
+            FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+            QuitMenuItem = true; # enable quit menu item
+            ShowPathbar = true; # show path bar
+            ShowStatusBar = true; # show status bar
+            FXPreferredViewStyle = "clmv";
           };
 
           trackpad = {
-            Clicking = true;
-            TrackpadRightClick = true;
+            # tap - 轻触触摸板, click - 点击触摸板
+            Clicking = true; # enable tap to click(轻触触摸板相当于点击)
+            TrackpadRightClick = true; # enable two finger right click
+            TrackpadThreeFingerDrag = true; # enable three finger drag
+          };
+
+          # customize settings that not supported by nix-darwin directly
+          # see the source code of https://github.com/rgcr/m-cli to get all the available options
+          CustomUserPreferences = {
+            ".GlobalPreferences" = {
+              # automatically switch to a new space when switching to the application
+              AppleSpacesSwitchOnActivate = true;
+            };
+            NSGlobalDomain = {
+              # Add a context menu item for showing the Web Inspector in web views
+              WebKitDeveloperExtras = true;
+            };
+            "com.apple.finder" = {
+              ShowExternalHardDrivesOnDesktop = true;
+              ShowHardDrivesOnDesktop = true;
+              ShowMountedServersOnDesktop = true;
+              ShowRemovableMediaOnDesktop = true;
+              _FXSortFoldersFirst = true;
+              # When performing a search, search the current folder by default
+              FXDefaultSearchScope = "SCcf";
+            };
+            "com.apple.desktopservices" = {
+              # Avoid creating .DS_Store files on network or USB volumes
+              DSDontWriteNetworkStores = true;
+              DSDontWriteUSBStores = true;
+            };
+            "com.apple.screensaver" = {
+              # Require password immediately after sleep or screen saver begins
+              askForPassword = 1;
+              askForPasswordDelay = 0;
+            };
+            "com.apple.screencapture" = {
+              location = "~/Desktop";
+              type = "png";
+            };
+            "com.apple.AdLib" = {
+              allowApplePersonalizedAdvertising = false;
+            };
+            # Prevent Photos from opening automatically when devices are plugged in
+            "com.apple.ImageCapture".disableHotPlug = true;
+          };
+
+          loginwindow = {
+            GuestEnabled = false; # disable guest user
+            SHOWFULLNAME = true; # show full name in login window
           };
         };
 
