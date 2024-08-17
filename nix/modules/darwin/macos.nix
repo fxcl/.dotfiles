54ae821a758d4a54d87032b1e8ad59b7d1cc2d28
okk <me@gnux.cn>
2024-08-17 19:49:24 +0800

    mac defaults app

--- nix/modules/darwin/macos.nix
+++ nix/modules/darwin/macos.nix
@@ -319,7 +319,7 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
           defaults write com.apple.screensaver askForPasswordDelay -int 0
 
           # Save screenshots to the desktop
-          defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+          defaults write com.apple.screencapture location -string "~/Desktop"
 
           # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
           defaults write com.apple.screencapture type -string "png"
@@ -420,6 +420,16 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
           # Dock, Dashboard, and hot corners                                            #
           ###############################################################################
 
+          # Enable highlight hover effect for the grid view of a stack (Dock)
+          defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+          # Set the icon size of Dock items to 36 pixels
+          defaults write com.apple.dock tilesize -int 36
+
+          # Change minimize/maximize window effect
+          defaults write com.apple.dock mineffect -string "scale"
+
+
           # Change minimize/maximize window effect
           defaults write com.apple.dock mineffect -string "scale"
 
@@ -432,13 +442,85 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
           # Show indicator lights for open applications in the Dock
           defaults write com.apple.dock show-process-indicators -bool true
 
+          # Dock orientation: 'left', 'bottom', 'right'
+          defaults write com.apple.dock 'orientation' -string 'left'
+
+          # Wipe all (default) app icons from the Dock
+          # This is only really useful when setting up a new Mac, or if you don’t use
+          # the Dock to launch apps.
+          #defaults write com.apple.dock persistent-apps -array
+
+          # Show only open applications in the Dock
+          defaults write com.apple.dock static-only -bool true
+
+          # Don’t animate opening applications from the Dock
+          defaults write com.apple.dock launchanim -bool false
+
+          # Speed up Mission Control animations
+          defaults write com.apple.dock expose-animation-duration -float 0.1
+
+          # Don’t group windows by application in Mission Control
+          # (i.e. use the old Exposé behavior instead)
+          defaults write com.apple.dock expose-group-by-app -bool true
+
+          # Disable Dashboard
+          defaults write com.apple.dashboard mcx-disabled -bool true
+
+          # Don’t show Dashboard as a Space
+          defaults write com.apple.dock dashboard-in-overlay -bool true
+
+          # Don’t automatically rearrange Spaces based on most recent use
+          defaults write com.apple.dock mru-spaces -bool false
+
+          # Remove the auto-hiding Dock delay
+          defaults write com.apple.dock autohide-delay -float 0
+          # Remove the animation when hiding/showing the Dock
+          defaults write com.apple.dock autohide-time-modifier -float 0
+
+          # Automatically hide and show the Dock
+          defaults write com.apple.dock autohide -bool true
+
+
           # Make Dock icons of hidden applications translucent
           defaults write com.apple.dock showhidden -bool true
+
+          # Disable the Launchpad gesture (pinch with thumb and three fingers)
+          #defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+
+          # Reset Launchpad, but keep the desktop wallpaper intact
+          find "~/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
 
 
           # Add iOS & Watch Simulator to Launchpad
           sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
+          #sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator (Watch).app" "/Applications/Simulator (Watch).app"
 
+          # Add a spacer to the left side of the Dock (where the applications are)
+          #defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+          # Add a spacer to the right side of the Dock (where the Trash is)
+          #defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
+
+          # Hot corners
+          # Possible values:
+          #  0: no-op
+          #  2: Mission Control
+          #  3: Show application windows
+          #  4: Desktop
+          #  5: Start screen saver
+          #  6: Disable screen saver
+          #  7: Dashboard
+          # 10: Put display to sleep
+          # 11: Launchpad
+          # 12: Notification Center
+          # Top left screen corner → Mission Control
+          defaults write com.apple.dock wvous-tl-corner -int 2
+          defaults write com.apple.dock wvous-tl-modifier -int 0
+          # Top right screen corner → Desktop
+          defaults write com.apple.dock wvous-tr-corner -int 4
+          defaults write com.apple.dock wvous-tr-modifier -int 0
+          # Bottom left screen corner → Start screen saver
+          defaults write com.apple.dock wvous-bl-corner -int 5
+          defaults write com.apple.dock wvous-bl-modifier -int 0
 
           ###############################################################################
           # Terminal                                                                    #
@@ -447,6 +529,30 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
           # Only use UTF-8 in Terminal.app
           defaults write com.apple.terminal StringEncodings -array 4
 
+          # Enable “focus follows mouse” for Terminal.app and all X11 apps
+          # i.e. hover over a window and start typing in it without clicking first
+          #defaults write com.apple.terminal FocusFollowsMouse -bool true
+          #defaults write org.x.X11 wm_ffm -bool true
+
+          # Enable Secure Keyboard Entry in Terminal.app
+          # See: https://security.stackexchange.com/a/47786/8918
+          defaults write com.apple.terminal SecureKeyboardEntry -bool true
+
+          # Disable the annoying line marks
+          defaults write com.apple.Terminal ShowLineMarks -int 0
+
+          # Don’t display the annoying prompt when quitting iTerm
+          defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+          ###############################################################################
+          # Time Machine                                                                #
+          ###############################################################################
+
+          # Prevent Time Machine from prompting to use new hard drives as backup volume
+          defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+          # Disable local Time Machine backups
+          hash tmutil &> /dev/null && sudo tmutil disablelocal
 
           ###############################################################################
           # Activity Monitor                                                            #
@@ -454,6 +560,9 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
           # Show the main window when launching Activity Monitor
           defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+          # Visualize CPU usage in the Activity Monitor Dock icon
+          defaults write com.apple.ActivityMonitor IconType -int 5
 
           # Show all processes in Activity Monitor
           defaults write com.apple.ActivityMonitor ShowCategory -int 0
@@ -482,6 +591,32 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
           # Check for software updates daily, not just once per week
           defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
+          # Enable the WebKit Developer Tools in the Mac App Store
+          defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+          # Enable Debug Menu in the Mac App Store
+          defaults write com.apple.appstore ShowDebugMenu -bool true
+
+          # Enable the automatic update check
+          defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+          # Check for software updates daily, not just once per week
+          defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+          # Download newly available updates in background
+          defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+          # Install System data files & security updates
+          defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+          # Automatically download apps purchased on other Macs
+          defaults write com.apple.SoftwareUpdate ConfigDataInstall -int 1
+
+          # Turn on app auto-update
+          defaults write com.apple.commerce AutoUpdate -bool true
+
+          # Allow the App Store to reboot machine on macOS updates
+          # defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
 
           ###############################################################################
           # Photos                                                                      #
@@ -489,6 +624,152 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
           # Prevent Photos from opening automatically when devices are plugged in
           defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+          ###############################################################################
+          # Messages                                                                    #
+          ###############################################################################
+
+          # Disable automatic emoji substitution (i.e. use plain text smileys)
+          defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
+
+          # Disable smart quotes as it’s annoying for messages that contain code
+          defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+
+          # Disable continuous spell checking
+          defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+
+          ###############################################################################
+          # Google Chrome & Google Chrome Canary                                        #
+          ###############################################################################
+
+          # Disable the all too sensitive backswipe on trackpads
+          defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+          defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+
+          # Disable the all too sensitive backswipe on Magic Mouse
+          defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+          defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
+
+          # Use the system-native print preview dialog
+          defaults write com.google.Chrome DisablePrintPreview -bool true
+          defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+          # Expand the print dialog by default
+          defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+          defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+
+          ###############################################################################
+          # Safari & WebKit                                                             #
+          ###############################################################################
+
+          # Privacy: don’t send search queries to Apple
+          defaults write com.apple.Safari UniversalSearchEnabled -bool false
+          defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
+          # Press Tab to highlight each item on a web page
+          defaults write com.apple.Safari WebKitTabToLinksPreferenceKey -bool true
+          defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks -bool true
+
+          # Show the full URL in the address bar (note: this still hides the scheme)
+          defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+
+          # Set Safari’s home page to `about:blank` for faster loading
+          defaults write com.apple.Safari HomePage -string "about:blank"
+
+          # Prevent Safari from opening ‘safe’ files automatically after downloading
+          defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+          # Allow hitting the Backspace key to go to the previous page in history
+          defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+          # Hide Safari’s bookmarks bar by default
+          defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+          # Hide Safari’s sidebar in Top Sites
+          defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+          # Disable Safari’s thumbnail cache for History and Top Sites
+          defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+          # Enable Safari’s debug menu
+          defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+          # Make Safari’s search banners default to Contains instead of Starts With
+          defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+          # Remove useless icons from Safari’s bookmarks bar
+          defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+          # Enable the Develop menu and the Web Inspector in Safari
+          defaults write com.apple.Safari IncludeDevelopMenu -bool true
+          defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+          defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+          # Add a context menu item for showing the Web Inspector in web views
+          defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+          # Enable continuous spellchecking
+          defaults write com.apple.Safari WebContinuousSpellCheckingEnabled -bool true
+          # Disable auto-correct
+          defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+
+          # Disable AutoFill
+          defaults write com.apple.Safari AutoFillFromAddressBook -bool false
+          defaults write com.apple.Safari AutoFillPasswords -bool false
+          defaults write com.apple.Safari AutoFillCreditCardData -bool false
+          defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
+
+          # Warn about fraudulent websites
+          defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
+
+          # Disable plug-ins
+          defaults write com.apple.Safari WebKitPluginsEnabled -bool false
+          defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
+
+          # Disable Java
+          defaults write com.apple.Safari WebKitJavaEnabled -bool false
+          defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
+
+          # Block pop-up windows
+          defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
+          defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
+
+          # Disable auto-playing video
+          #defaults write com.apple.Safari WebKitMediaPlaybackAllowsInline -bool false
+          #defaults write com.apple.SafariTechnologyPreview WebKitMediaPlaybackAllowsInline -bool false
+          #defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2AllowsInlineMediaPlayback -bool false
+          #defaults write com.apple.SafariTechnologyPreview com.apple.Safari.ContentPageGroupIdentifier.WebKit2AllowsInlineMediaPlayback -bool false
+
+          # Enable “Do Not Track”
+          defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+
+          # Update extensions automatically
+          defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
+
+          ###############################################################################
+          # Mail                                                                        #
+          ###############################################################################
+
+          # Disable send and reply animations in Mail.app
+          defaults write com.apple.mail DisableReplyAnimations -bool true
+          defaults write com.apple.mail DisableSendAnimations -bool true
+
+          # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+          defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+          # Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
+          defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\U21a9"
+
+          # Display emails in threaded mode, sorted by date (oldest at the top)
+          defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
+          defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
+          defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
+
+          # Disable inline attachments (just show the icons)
+          defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+
+          # Disable automatic spell checking
+          defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
 
           ###############################################################################
           # Kill affected applications                                                  #
