4fd688b938d3024150a4fe12b65d2dcc50af8a8c
okk <me@gnux.cn>
2024-08-17 19:55:14 +0800

    remove disblelocal

--- nix/modules/darwin/macos.nix
+++ nix/modules/darwin/macos.nix
@@ -548,10 +548,6 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
           # Prevent Time Machine from prompting to use new hard drives as backup volume
           defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-          # Disable local Time Machine backups
-          hash tmutil &> /dev/null && sudo tmutil disablelocal
-
-          ###############################################################################
           # Activity Monitor                                                            #
           ###############################################################################
 
